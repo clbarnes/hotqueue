c1af351cee45d1b2e78ab886afdada3ed0dce04c
Chris Barnes <barnesc@janelia.hhmi.org>
2018-08-20 10:40:11 -0400

    Change .__redis to ._redis
    Using the class-mangled name prevents subclassing and adds no benefit in
    a world of consenting adults.

--- hotqueue.py
+++ hotqueue.py
@@ -16,6 +16,7 @@ from redis import Redis
 __all__ = ['HotQueue']
 
 __version__ = '0.2.8'
+__version_info__ = tuple(int(v) for v in __version__.split('.'))
 
 
 def key_for_name(name):
@@ -43,10 +44,10 @@ class HotQueue(object):
     def __init__(self, name, serializer=pickle, **kwargs):
         self.name = name
         self.serializer = serializer
-        self.__redis = Redis(**kwargs)
+        self._redis = Redis(**kwargs)
     
     def __len__(self):
-        return self.__redis.llen(self.key)
+        return self._redis.llen(self.key)
     
     @property
     def key(self):
@@ -55,7 +56,7 @@ class HotQueue(object):
     
     def clear(self):
         """Clear the queue of all messages, deleting the Redis key."""
-        self.__redis.delete(self.key)
+        self._redis.delete(self.key)
     
     def consume(self, **kwargs):
         """Return a generator that yields whenever a message is waiting in the
@@ -95,11 +96,11 @@ class HotQueue(object):
         if block:
             if timeout is None:
                 timeout = 0
-            msg = self.__redis.blpop(self.key, timeout=timeout)
+            msg = self._redis.blpop(self.key, timeout=timeout)
             if msg is not None:
                 msg = msg[1]
         else:
-            msg = self.__redis.lpop(self.key)
+            msg = self._redis.lpop(self.key)
         if msg is not None and self.serializer is not None:
             msg = self.serializer.loads(msg)
         return msg
@@ -117,7 +118,7 @@ class HotQueue(object):
         """
         if self.serializer is not None:
             msgs = map(self.serializer.dumps, msgs)
-        self.__redis.rpush(self.key, *msgs)
+        self._redis.rpush(self.key, *msgs)
     
     def worker(self, *args, **kwargs):
         """Decorator for using a function as a queue worker. Example:

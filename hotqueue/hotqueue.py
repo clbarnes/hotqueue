c78162893c2fffcd05348e701c8879d544706573
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-03 17:09:42 -0400

    Fix PEP8 violations

--- hotqueue/hotqueue.py
+++ hotqueue/hotqueue.py
@@ -33,8 +33,9 @@ class HotQueue(object):
         `pickle <http://docs.python.org/library/pickle.html>`_ is the default,
         use ``None`` to store messages in plain text (suitable for strings,
         integers, etc).
-        Consider using a hotqueue.PickleSerializer, which uses the newest pickle
-        protocol available, rather than a lower version for compatibility.
+        Consider using a hotqueue.PickleSerializer, which uses the newest
+        pickle protocol available, rather than a lower version for
+        compatibility.
 
     redis : redis.Redis, redislite.Redis, optional
         redis connection object, defaults to redislite.Redis with fallback to

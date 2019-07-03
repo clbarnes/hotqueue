cfb3101800ec45ba50b4be0ef0540069f1362030
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-03 16:55:28 -0400

    Update __init__.__all__ and __version_info__

--- hotqueue/__init__.py
+++ hotqueue/__init__.py
@@ -48,9 +48,9 @@ Example:
 import json
 import os
 from .hotqueue import HotQueue
+from .serializers import PickleSerializer
 
-
-__all__ = ['hotqueue']
+__all__ = ['HotQueue', 'PickleSerializer']
 __version__ = str('0.0.0')
 __git_version__ = str("")
 __git_origin__ = str("")
@@ -77,3 +77,12 @@ if os.path.exists(_metadata_file):  # pragma: no cover
         if __git_origin__.endswith('.git'):  # pragma: no cover
             __git_base_url__ = __git_origin__[:-4].strip('/')
         __source_url__ = __git_base_url__ + '/tree/' + __git_hash__
+
+
+__version_info__ = tuple()
+for item in __version__:
+    try:
+        item = int(item)
+    except (ValueError, TypeError):
+        pass
+    __version_info__ += (item,)
